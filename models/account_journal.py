909589550f33460d292022423e923484c4d5eadd
Adr-oc <orantesadrian01@gmail.com>
2025-12-10 08:50:48 -0600

    hotfix

--- models/account_journal.py
+++ models/account_journal.py
@@ -8,10 +8,7 @@ class AccountJournal(models.Model):
     _inherit = "account.journal"
 
 
-    
-
     bank_instruccion = fields.Many2one('res.partner.bank', "Instrucción de banco")
-    # NOTA: partner_id ya existe como Many2one en account.journal base
-    # No se debe redefinir como Char. Campo eliminado para evitar conflictos.
+    partner_id = fields.Many2one('res.partner', string="Partner", help="Partner for this journal")
 
     
